cf80c62592907c30d05e6d5570ce60694a866195
juanpereiira <juanjudep@gmail.com>
2025-10-11 23:11:31 +0530

    Testing Backend for production

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -18,7 +18,7 @@ function ControlPanel({ onSpawn }) {
     };
 
     // --- THIS IS THE REAL API CALL ---
-    fetch('http://localhost:5000/simulate-impact', {
+    fetch('http://astro-impact.onrender.com/simulate-impact', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload),
